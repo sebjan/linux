46e15ab08961e9ee4c899eefea15f5d3b06d188c
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-07-24 20:59:34 +0100

    Subject: [PATCH 039/104] OMAP3: Move common twl configuration to twl-common
    Reduce the amount of duplicated code by moving the common
    configuration for twl4030/5030/tpsxx to the twl-common file.
    Use the omap3_pmic_get_config function from board files to
    properly configure the PMIC with the common fields.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/common-board-devices.h
+++ arch/arm/mach-omap2/common-board-devices.h
@@ -4,6 +4,9 @@
 #define NAND_BLOCK_SIZE	SZ_128K
 
 #define TWL_COMMON_PDATA_USB		(1 << 0)
+#define TWL_COMMON_PDATA_BCI		(1 << 1)
+#define TWL_COMMON_PDATA_MADC		(1 << 2)
+#define TWL_COMMON_PDATA_AUDIO		(1 << 3)
 
 /* Common LDO regulators for TWL4030/TWL6030 */
 #define TWL_COMMON_REGULATOR_VDAC	(1 << 0)
@@ -50,6 +53,8 @@ struct ads7846_platform_data;
 void omap_ads7846_init(int bus_num, int gpio_pendown, int gpio_debounce,
 		       struct ads7846_platform_data *board_pdata);
 void omap_nand_flash_init(int opts, struct mtd_partition *parts, int n_parts);
+void omap3_pmic_get_config(struct twl4030_platform_data *pmic_data,
+			   u32 pdata_flags, u32 regulators_flags);
 
 void omap4_pmic_get_config(struct twl4030_platform_data *pmic_data,
 			   u32 pdata_flags, u32 regulators_flags);
