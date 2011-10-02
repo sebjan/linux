b0d6962092692d75e22cc19cad1f9369debdb0d5
Hemant Hariyani <hemanthariyani@ti.com>
2011-10-02 16:19:17 +0100

    Kernel changes for hwmod and omap_device initialization for GPU.
    Change-Id: I4bd96e76af31d0991ce9e67b0c499f0d6f39a1a8
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -56,6 +56,7 @@ static struct omap_hwmod omap44xx_emif_fw_hwmod;
 static struct omap_hwmod omap44xx_hsi_hwmod;
 static struct omap_hwmod omap44xx_ipu_hwmod;
 static struct omap_hwmod omap44xx_iss_hwmod;
+static struct omap_hwmod omap44xx_gpu_hwmod;
 static struct omap_hwmod omap44xx_iva_hwmod;
 static struct omap_hwmod omap44xx_l3_instr_hwmod;
 static struct omap_hwmod omap44xx_l3_main_1_hwmod;
@@ -358,6 +359,14 @@ static struct omap_hwmod_ocp_if omap44xx_hsi__l3_main_2 = {
 	.user		= OCP_USER_MPU | OCP_USER_SDMA,
 };
 
+/* gpu -> l3_main_2 */                                                          
+static struct omap_hwmod_ocp_if omap44xx_gpu__l3_main_2 = {                     
+        .master         = &omap44xx_gpu_hwmod,                                  
+        .slave          = &omap44xx_l3_main_2_hwmod,                            
+        .clk            = "l3_div_ck",                                          
+        .user           = OCP_USER_MPU | OCP_USER_SDMA,                         
+};  
+
 /* ipu -> l3_main_2 */
 static struct omap_hwmod_ocp_if omap44xx_ipu__l3_main_2 = {
 	.master		= &omap44xx_ipu_hwmod,
@@ -422,6 +431,7 @@ static struct omap_hwmod_ocp_if *omap44xx_l3_main_2_slaves[] = {
 	&omap44xx_hsi__l3_main_2,
 	&omap44xx_ipu__l3_main_2,
 	&omap44xx_iss__l3_main_2,
+	&omap44xx_gpu__l3_main_2,
 	&omap44xx_iva__l3_main_2,
 	&omap44xx_l3_main_1__l3_main_2,
 	&omap44xx_l4_cfg__l3_main_2,
@@ -2611,6 +2621,75 @@ static struct omap_hwmod omap44xx_gpio6_hwmod = {
 	.slaves		= omap44xx_gpio6_slaves,
 	.slaves_cnt	= ARRAY_SIZE(omap44xx_gpio6_slaves),
 	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_OMAP44XX),
+};
+
+/*                                                                              
+ * 'gpu' class                                                                  
+ * 2d/3d graphics accelerator                                                   
+ */
+                                                                                
+static struct omap_hwmod_class_sysconfig omap44xx_gpu_sysc = {                  
+        .rev_offs       = 0xfe00,                                               
+        .sysc_offs      = 0xfe10,                                               
+        .sysc_flags     = (SYSC_HAS_MIDLEMODE | SYSC_HAS_SIDLEMODE),            
+        .idlemodes      = (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |               
+                           MSTANDBY_FORCE | MSTANDBY_NO | MSTANDBY_SMART),      
+        .sysc_fields    = &omap_hwmod_sysc_type2,                               
+};                                                                              
+                                                                                
+static struct omap_hwmod_class omap44xx_gpu_hwmod_class = {                     
+        .name = "gpu",                                                          
+        .sysc = &omap44xx_gpu_sysc,                                             
+};                                                                              
+                                                                                
+/* gpu */                                                                       
+static struct omap_hwmod_irq_info omap44xx_gpu_irqs[] = {                       
+        { .irq = 21 + OMAP44XX_IRQ_GIC_START },                                 
+};                                                                              
+                                                                                
+/* gpu master ports */                                                          
+static struct omap_hwmod_ocp_if *omap44xx_gpu_masters[] = {                     
+        &omap44xx_gpu__l3_main_2,                                               
+};                                                                              
+                                                                                
+static struct omap_hwmod_addr_space omap44xx_gpu_addrs[] = {                    
+        {                                                                       
+                .pa_start       = 0x56000000,                                   
+                .pa_end         = 0x5600ffff,                                   
+                .flags          = ADDR_TYPE_RT                                  
+        },                                                                      
+}; 
+
+/* l3_main_2 -> gpu */                                                          
+static struct omap_hwmod_ocp_if omap44xx_l3_main_2__gpu = {                     
+        .master         = &omap44xx_l3_main_2_hwmod,                            
+        .slave          = &omap44xx_gpu_hwmod,                                  
+        .clk            = "l3_div_ck",                                          
+        .addr           = omap44xx_gpu_addrs,                                   
+        .user           = OCP_USER_MPU | OCP_USER_SDMA,                         
+};                                                                              
+                                                                                
+/* gpu slave ports */                                                           
+static struct omap_hwmod_ocp_if *omap44xx_gpu_slaves[] = {                      
+        &omap44xx_l3_main_2__gpu,                                               
+};                                                                              
+                                                                                
+static struct omap_hwmod omap44xx_gpu_hwmod = {                                 
+        .name           = "gpu",                                                
+        .class          = &omap44xx_gpu_hwmod_class,                            
+        .mpu_irqs       = omap44xx_gpu_irqs,                                    
+        .main_clk       = "gpu_fck",                                            
+        /* .vdd_name       = "core", */
+        .prcm = {                                                               
+                .omap4 = {                                                      
+                        .clkctrl_offs = OMAP4_CM_GFX_GFX_CLKCTRL_OFFSET,             
+                },                                                              
+        },                                                                      
+        .slaves         = omap44xx_gpu_slaves,                                  
+        .slaves_cnt     = ARRAY_SIZE(omap44xx_gpu_slaves),                      
+        .masters        = omap44xx_gpu_masters,                                 
+        .masters_cnt    = ARRAY_SIZE(omap44xx_gpu_masters),                     
+        .omap_chip      = OMAP_CHIP_INIT(CHIP_IS_OMAP44XX),                     
 };
 
 /*
@@ -5612,6 +5691,9 @@ static __initdata struct omap_hwmod *omap44xx_hwmods[] = {
 
 	/* hsi class */
 /*	&omap44xx_hsi_hwmod, */
+
+	/* gpu class */
+	&omap44xx_gpu_hwmod,
 
 	/* i2c class */
 	&omap44xx_i2c1_hwmod,
