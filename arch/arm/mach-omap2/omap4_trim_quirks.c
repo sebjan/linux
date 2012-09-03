imoseyon/leanKernel-galaxy-nexus

OMAP4: trim quirk: Fix incorrect CPU revision check
Fix incorrect check CPU revision during check for eFUSE trim
as some early samples of CPUs revs ES 2.2 and ES 2.3 4430 and
4460s also need the "s/w workaround".

Since untrimmed parts are not really supposed to be seen on
the field, put a message on log to search for.

[nm@ti.com: pulled back into k3.0 from k35]
Signed-off-by: Nishanth Menon <nm@ti.com>
Signed-off-by: Volodymyr Riazantsev <x0153367@ti.com>

arch/arm/mach-omap2/omap4_trim_quirks.c
@@ -40,12 +40,13 @@ static int __init omap4_ldo_trim_configure(void)
 	 * 2. trim VDAC value for TV output as per recomendation
 	 */
 
-	if (omap_rev() == CHIP_IS_OMAP4430ES2_2)
+	if (omap_rev() >= CHIP_IS_OMAP4430ES2_2)
 		is_trimmed = omap_ctrl_readl(
 			OMAP4_CTRL_MODULE_CORE_LDOSRAM_MPU_VOLTAGE_CTRL);
 
 	/* if not trimmed, we set force overide, insted of efuse. */
 	if (!is_trimmed) {
+		pr_err("%s: UNTRIMMED PART\n", __func__);
 		/* Fill in recommended values */
 		val = 0x0f << OMAP4_LDOSRAMCORE_ACTMODE_VSET_OUT_SHIFT;
 		val |= OMAP4_LDOSRAMCORE_ACTMODE_MUX_CTRL_MASK;

sebjan/linux

arch/arm/mach-omap2/omap4_trim_quirks.c
@@ -40,12 +40,13 @@ static int __init omap4_ldo_trim_configure(void)
 	 * 2. trim VDAC value for TV output as per recomendation
 	 */
 
-	if (omap_rev() == OMAP4430_REV_ES2_2)
+	if (omap_rev() >= OMAP4430_REV_ES2_2)
 		is_trimmed = omap_ctrl_readl(
 			OMAP4_CTRL_MODULE_CORE_LDOSRAM_MPU_VOLTAGE_CTRL);
 
 	/* if not trimmed, we set force overide, insted of efuse. */
 	if (!is_trimmed) {
+		pr_err("%s: UNTRIMMED PART\n", __func__);
 		/* Fill in recommended values */
 		val = 0x0f << OMAP4_LDOSRAMCORE_ACTMODE_VSET_OUT_SHIFT;
 		val |= OMAP4_LDOSRAMCORE_ACTMODE_MUX_CTRL_MASK;